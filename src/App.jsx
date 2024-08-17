9f6eb38a52911428de868d1091057b1acb02288c
Idera Nimah <iderayusuf29@gmail.com>
2024-08-17 15:05:19 +0100

    starter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import BG from "./assets/bg-desktop.png";
+
+
+const App = () => {
+  return (
+    <div className="">
+      <img src={BG} alt="background-image" />
+      {/* Your content here */}
+    </div>
+  );
+};
+
+export default App;
