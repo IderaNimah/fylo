bf3e15337d0d1100f5405de1188fc08967fce7c1
Idera Nimah <iderayusuf29@gmail.com>
2024-08-17 16:16:26 +0100

    component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,30 @@ import BG from "./assets/bg-desktop.png";
 
 const App = () => {
   return (
+    <>
     <div className="starter">
       <img src={BG} alt="background-image" />
     </div>
+    <StarterOne/>
+
+    </>   
   );
+ 
 };
 
+function StarterOne(){
+  return(
+  <div className="text">
+   <h1>
+    Fylo
+   </h1>
+   </div>
+  )
+};
+
+// function StarterTwo(){
+//   return(
+
+//   )
+// };
 export default App;
