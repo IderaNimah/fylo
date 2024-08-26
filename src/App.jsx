278111b283435e7211f918146b094ea2ef3fe3d1
Idera Nimah <iderayusuf29@gmail.com>
2024-08-26 13:05:28 +0100

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,77 @@
+import React, { useState } from 'react';
 import "./App.css";
 import BG from "./assets/bg-desktop.png";
-
 
 const App = () => {
   return (
     <>
-    <div className="starter">
-      <img src={BG} alt="background-image" />
-    </div>
-    <StarterOne/>
-    <StarterTwo/>
-    </>   
+      <div className="starter">
+        <img src={BG} alt="background-image" />
+      </div>
+      <StarterOne />
+      <StarterTwo />
+    </>
   );
- 
 };
 
-function StarterOne(){
-  return(
-  <div className="text">
-   <h1>
-    <img src="./images/logo.svg" alt=""/>
-   </h1>
-   <Icon/>
-   </div>
-  )
-};
-
-function Icon(){
-return(
-  <div className="icons">
-    <>
-    <img src="./images/icon-document.svg"/>
-    </>
-    <>
-    <img src="./images/icon-folder.svg"/>
-    </>
-    <>
-    <img src="./images/icon-upload.svg"/>
-    </>
-  </div>
-)
-};
-
-function StarterTwo(){
-  return(
-  <div className="second">
-    <h3>You've used <span className="diff">815GB</span> of your storage</h3>
-    <Data/>
-  </div>
-  )
-};
-
-function Data(){
-  return(
-    <div className="datavisual">
-     <h1>185<span>GB left</span></h1>
+function StarterOne() {
+  return (
+    <div className="text">
+      <h1>
+        <img src="./images/logo.svg" alt="" />
+      </h1>
+      <Icon />
     </div>
-  )
+  );
 }
+
+function Icon() {
+  return (
+    <div className="icons">
+      <img src="./images/icon-document.svg" alt="Document Icon" />
+      <img src="./images/icon-folder.svg" alt="Folder Icon" />
+      <img src="./images/icon-upload.svg" alt="Upload Icon" />
+    </div>
+  );
+}
+
+function StarterTwo() {
+  const [sliderValue, setSliderValue] = useState(815); // Initial value
+
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+  };
+
+  return (
+    <div className="second">
+      <h3>
+        You’ve used <span className="diff">{sliderValue}GB</span> of your storage
+      </h3>
+      <div className="slider-container">
+        <input
+          type="range"
+          min="0"
+          max="1000" // Max storage value
+          value={sliderValue}
+          onChange={handleSliderChange}
+          className="slider"
+        />
+      </div>
+      <Data sliderValue={sliderValue} />
+    </div>
+  );
+}
+
+function Data({ sliderValue }) {
+  const maxStorage = 1000; // Example max storage in GB
+
+  return (
+    <div className="datavisual">
+      <h1>
+        {maxStorage - sliderValue} <span className="tiny">GB left</span>
+      </h1>
+    </div>
+  );
+}
+
 export default App;
